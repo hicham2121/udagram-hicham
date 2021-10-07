6c75995db93592f7182bc57f3aae46574ea7c6f0
hicham2121 <bnrq.hicham@gmail.com>
2021-10-07 05:49:10 +0100

    review

--- src/server.ts
+++ src/server.ts
@@ -1,84 +1,63 @@
-import express, { response } from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles, filterImageFromURLWithMethod } from './util/util';
-import { nextTick } from 'process';
-import { Router, Request, Response } from 'express';
+import express, { NextFunction, Router, Request, Response } from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  
- 
-
-  
-  app.get( "/filteredimage", async ( req, res ) => {
-
-    console.log("============= default 1============== ");
-    
+  app.get(
+    "/filteredimage",
+    async (req: Request, res: Response, next: NextFunction) => {
+      console.log("============= default 1============== ");
 
       //get the url from the query
-      const image_url = req.query.image_url;
+      let image_url: string = (await req.query.image_url) as string;
 
       // 1. validate the image_url query
       if (!image_url) {
-        return res.status(404).send({message: 'no valide image url, '});
+        return res.status(422).send({ message: "no valide image url, " });
       }
       console.log("======== image_url ==========");
       console.log(image_url);
 
-      let  resultFiltredmagePath : string;
+      let absolutePath: string;
       // 2. call filterImageFromURL(image_url) to filter the image
-      try{
-        resultFiltredmagePath = await filterImageFromURL(image_url);
+      try {
+        absolutePath = await filterImageFromURL(image_url);
         console.log("======= resultFiltredmagePath =======");
-        console.log(resultFiltredmagePath);
+        console.log(absolutePath);
+        // 3. send the resulting file in the response
+        return await res.status(200).sendFile(absolutePath, (error: Error) => {
+          deleteLocalFiles([absolutePath]);
+          if (error) {
+            //no such file or directory,
 
-      }catch(error){
-
-        return res.status(415).send({message: error});
-
-
+            return res.status(400).send(error.message);
+          }
+        });
+      } catch (error) {
+        return next(error);
       }
-
-      // 3. send the resulting file in the response       
-      res.sendFile(resultFiltredmagePath,  (err)=> {
-
-        // 4. deletes any files on the server on finish of the response
-         deleteLocalFiles([resultFiltredmagePath]);
-
-        if (err) {  
-
-          //no such file or directory,
-
-          return res.status(400).send(err.message);
-        }  
-    });
-
- 
-     
-  } );
-
-  
+    }
+  );
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import Jimp = require('jimp');
+import fs from "fs";
+import Jimp = require("jimp");
 import axios from "axios";
 
 // filterImageFromURL
@@ -7,27 +7,21 @@ import axios from "axios";
 // returns the absolute path to the local image
 // INPUTS
 //    inputURL: string - a publicly accessible url to an image file
-// RETURNs 
+// RETURNs
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-         
-
-        
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        // .blur(20)
-        // .invert()
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-
-        });
-         
-    });
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+  return new Promise(async (resolve) => {
+    const photo = await Jimp.read(inputURL);
+    const outpath =
+      "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+    await photo
+      .resize(256, 256) // resize
+      .quality(60) // set JPEG quality
+      .greyscale()
+      .write(__dirname + outpath, (img) => {
+        resolve(__dirname + outpath);
+      });
+  });
 }
 
 // deleteLocalFiles
@@ -35,37 +29,8 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
-        fs.unlinkSync(file);
-    }
-}
-
-
-export async function filterImageFromURLWithMethod(inputURL: string, methodName:String): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        
-
-        switch (methodName) {
-            case "invert":
-                photo.invert();           
-             case "greyscale":
-                photo.grayscale();
-            case "blur":
-                photo.blur(10);            
-            default:
-                photo.grayscale();
-        }
-                
-        // .invert()
-        //.greyscale() // set greyscale
-        photo.write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
-    });
+export async function deleteLocalFiles(files: Array<string>) {
+  for (let file of files) {
+    fs.unlinkSync(file);
+  }
 }
